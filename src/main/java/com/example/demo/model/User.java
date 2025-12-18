bac08b1b61628d0589043c72853ac7585088609f
bala-amypo <bala@amypo.in>
2025-12-18 04:07:56 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,10 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
 @Entity
-public class ComplianceRule{
+public class User{
     @id
     private long id;
+    @Column(name="email",)
     private String fullname;
     private String email;
     private LocalDate createdAt;
