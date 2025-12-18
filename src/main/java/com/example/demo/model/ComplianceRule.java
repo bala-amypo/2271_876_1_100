504d06825694b695188066a7f5ca34699e68dbd4
bala-amypo <bala@amypo.in>
2025-12-18 03:42:55 +0000

    User commit

--- src/main/java/com/example/demo/model/ComplianceRule.java
+++ src/main/java/com/example/demo/model/ComplianceRule.java
@@ -1,4 +1,8 @@
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+@Entity
 public class ComplianceRule{
+    @id
     private long id;
     private String fullname;
     private String email;
