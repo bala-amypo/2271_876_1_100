b55e69a7d6f16b369e624533b14418c2685cfcfc
bala-amypo <bala@amypo.in>
2025-12-25 11:57:50 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HealthServlet.java
+++ src/main/java/com/example/demo/servlet/HealthServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/health/*")
+@WebServlet(urlPatterns = {"/health/*"})
 public class HealthServlet extends HttpServlet {
 
     public HealthServlet() {
